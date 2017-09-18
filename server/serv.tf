48f87eb56ff01ff3091aee5d690e1fa17bbae908
root <root@localhost.localdomain>
2017-09-18 12:29:41 -0400

    NEW commit

--- server/serv.tf
+++ server/serv.tf
@@ -1,9 +1,11 @@
 
 
 resource "aws_instance" "pub_example" {
-  ami           = "ami-501b5f3f"
+#  ami           = "ami-501b5f3f"
+  ami           = "ami-c998b6b2"
   instance_type = "${var.modname}"
-  key_name = "docker key"
+  key_name = "terra"
+  #key_name = "docker key"
   vpc_security_group_ids = ["${var.awssecgp}"]
   subnet_id = "${var.awssub}"
   tags {
